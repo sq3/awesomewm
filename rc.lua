86f81290467f13166636f19d95e69ca29b70a622
Unknown <accounts@mschnitzius.com>
2018-04-01 12:04:29 +0200

    change layouts
    using the layouts that works with the new layout icons

--- rc.lua
+++ rc.lua
@@ -90,11 +90,11 @@ local tagnames   = {
 
 -- table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
+    awful.layout.suit.floating,
     awful.layout.suit.tile,
-    awful.layout.suit.fair,
+    awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
     awful.layout.suit.tile.top,
-    awful.layout.suit.floating,
     --awful.layout.suit.tile.left,
     --awful.layout.suit.fair.horizontal,
     --awful.layout.suit.spiral,
